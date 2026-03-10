40f5a5c1d5cb72b83eb0309b0e00e618cd094ef5
신윤섭 <zksdbstjq@gmail.com>
2026-03-10 12:10:49 +0900

    🎉 init: 프로젝트 초기 설정
    - Spring Boot 애플리케이션 기본 설정 추가
    - Gradle Wrapper 및 의존성 설정
    - 기본 디렉토리 구조 생성
    - .gitignore 및 .gitattributes 파일 추가
    
    Ref: #S14P21A406-76, #S14P21A406-77, #S14P21A406-80

---
+++ backend/src/main/java/com/backend/zud/ZudApplication.java
@@ -0,0 +1,13 @@
+package com.backend.zud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/backend/zud/ZudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.zud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
