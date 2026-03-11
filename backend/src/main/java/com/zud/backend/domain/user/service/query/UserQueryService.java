7b5779a5020e6c28568cdb19e923fa01fca53139
신윤섭 <zksdbstjq@gmail.com>
2026-03-11 23:31:05 +0900

    ✨ feat: 사용자 조회 서비스 및 레포지토리 추가
    Ref: #S14P21A406-56

---
+++ backend/src/main/java/com/zud/backend/domain/user/service/query/UserQueryService.java
@@ -0,0 +1,7 @@
+package com.zud.backend.domain.user.service.query;
+
+import com.zud.backend.domain.user.entity.User;
+
+public interface UserQueryService {
+	User findByEmployeeNumber(final String employeeNumber);
+}
