99241277bf86e1d91178a0cf996c79d25e70b35d
신윤섭 <zksdbstjq@gmail.com>
2026-03-11 23:32:17 +0900

    ✨ feat: 인증 서비스 및 세션 관리 추가
    Ref: #S14P21A406-56

---
+++ backend/src/main/java/com/zud/backend/common/util/CookieUtils.java
@@ -0,0 +1,35 @@
+package com.zud.backend.common.util;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import com.zud.backend.domain.auth.enums.SessionConstants;
+
+import jakarta.servlet.http.Cookie;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CookieUtils {
+
+	private static final String PATH = "/";
+
+	public Cookie generateSessionCookie(final String sessionId) {
+		Cookie cookie = new Cookie(SessionConstants.PREFIX, sessionId);
+		cookie.setPath(PATH);
+		ZonedDateTime seoulTime = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+		ZonedDateTime expirationTime = seoulTime.plusHours(SessionConstants.EXPIRATION_HOUR_TIME);
+		cookie.setMaxAge((int)(expirationTime.toEpochSecond() - seoulTime.toEpochSecond()));
+		cookie.setSecure(true);
+		cookie.setHttpOnly(true);
+		return cookie;
+	}
+
+	public Cookie deleteSessionCookie(final String sessionId) {
+		Cookie cookie = new Cookie(SessionConstants.PREFIX, sessionId);
+		cookie.setPath(PATH);
+		cookie.setMaxAge(0);
+		cookie.setSecure(true);
+		cookie.setHttpOnly(true);
+		return cookie;
+	}
+}
