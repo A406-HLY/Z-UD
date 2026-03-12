5b9772fc76aedc14f8b77b2aeefcc47e8adf29d0
신윤섭 <zksdbstjq@gmail.com>
2026-03-12 11:09:19 +0900

    ✨ feat: Branch 엔티티 및 관계 설정 추가
    Ref: #S14P21A406-56

--- backend/src/main/java/com/zud/backend/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/zud/backend/domain/user/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.zud.backend.domain.user.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.zud.backend.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	@EntityGraph(attributePaths = {"branch"})
 	Optional<User> findByEmployeeNumber(String employeeNumber);
 }
