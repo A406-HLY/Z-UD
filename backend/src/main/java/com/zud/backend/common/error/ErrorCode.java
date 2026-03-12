c522784abd6ef59e2f7592fe03ed8f6e2f37fd97
yehyeon <yhyh1216@gmail.com>
2026-03-12 18:13:16 +0900

    ✨ feat: 근처 은행 지점 조회 servcie 추가
    - 필요한 조회 결과만 담는 projection 생성
    - application.yml 카카오 api key 설정 추가
    - 필요한 ErrorCode 추가
    
    Ref: #S14P21A406-115

--- backend/src/main/java/com/zud/backend/common/error/ErrorCode.java
+++ backend/src/main/java/com/zud/backend/common/error/ErrorCode.java
@@ -39,7 +39,13 @@ public enum ErrorCode {
 	INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "US-001", "사번 또는 비밀번호가 일치하지 않습니다."),
 	ACCOUNT_LOCKED(HttpStatus.FORBIDDEN, "US-002", "로그인 시도 초과로 계정이 잠겼습니다."),
 	USER_NOT_REGISTERED(HttpStatus.FORBIDDEN, "US-003", "등록되지 않은 사용자입니다."),
-	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "US-004", "사용자를 찾을 수 없습니다.");
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "US-004", "사용자를 찾을 수 없습니다."),
+
+	/**
+	 * Branch Error (BR-xxx)
+	 */
+	ADDRESS_COORDINATE_NOT_FOUND(HttpStatus.NOT_FOUND, "BR-001", "주소에 대한 좌표를 찾을 수 없습니다."),
+	BRANCH_NOT_FOUND(HttpStatus.NOT_FOUND, "BR-002", "지점을 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
