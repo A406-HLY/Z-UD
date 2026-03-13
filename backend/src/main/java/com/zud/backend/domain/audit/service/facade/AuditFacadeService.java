e667b0102fd6b107498d965ab074d8b2bb20322b
yehyeon <yhyh1216@gmail.com>
2026-03-13 10:15:00 +0900

    ✨ feat: 심사 api 구현
    - 심사 API에서 branch 심사 진행
    
    Ref: #S14P21A406-115

---
+++ backend/src/main/java/com/zud/backend/domain/audit/service/facade/AuditFacadeService.java
@@ -0,0 +1,9 @@
+package com.zud.backend.domain.audit.service.facade;
+
+import com.zud.backend.domain.audit.dto.request.AuditReqDto;
+import com.zud.backend.domain.audit.dto.response.AuditResDto;
+
+public interface AuditFacadeService {
+	AuditResDto audit(final Long userId, final AuditReqDto reqDto);
+}
+
