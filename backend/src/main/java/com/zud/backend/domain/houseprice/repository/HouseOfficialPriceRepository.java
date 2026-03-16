7f72f8f0a44270a6fd186557cc750822eea4387b
yehyeon <yhyh1216@gmail.com>
2026-03-16 17:09:39 +0900

    ✨ feat: 근삿값 로직 수정
    - 근삿값 우선순위 적용 (동 또는 층(호)가 일치하는 경우 > 건물명 일치)
    
    Ref: #S14P21A406-120

--- backend/src/main/java/com/zud/backend/domain/houseprice/repository/HouseOfficialPriceRepository.java
+++ backend/src/main/java/com/zud/backend/domain/houseprice/repository/HouseOfficialPriceRepository.java
@@ -28,15 +28,29 @@ public interface HouseOfficialPriceRepository extends JpaRepository<HouseOfficia
 
 	@Query("""
 		SELECT h FROM HouseOfficialPrice h
-		WHERE h.sido = :sido
-		AND h.sigungu = :sigungu
-		AND (:dongRi IS NULL OR h.dongRi = :dongRi)
+		WHERE h.roadAddress = :roadAddress
+		AND h.complexName = :complexName
+		AND (:dongName IS NULL OR h.dongName = :dongName)
+		AND (:hoName IS NULL OR h.hoName = :hoName)
 		ORDER BY h.officialPrice ASC
 		LIMIT 5
 		""")
-	List<HouseOfficialPrice> findLowestPricesInDong(
-		@Param("sido") String sido,
-		@Param("sigungu") String sigungu,
-		@Param("dongRi") String dongRi
+	List<HouseOfficialPrice> findLowestPricesByBuildingDetail(
+		@Param("roadAddress") String roadAddress,
+		@Param("complexName") String complexName,
+		@Param("dongName") String dongName,
+		@Param("hoName") String hoName
+	);
+
+	@Query("""
+		SELECT h FROM HouseOfficialPrice h
+		WHERE h.roadAddress = :roadAddress
+		AND h.complexName = :complexName
+		ORDER BY h.officialPrice ASC
+		LIMIT 5
+		""")
+	List<HouseOfficialPrice> findLowestPricesByBuilding(
+		@Param("roadAddress") String roadAddress,
+		@Param("complexName") String complexName
 	);
 }
