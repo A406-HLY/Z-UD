8e7cf8f7814cfa72aac15edbf60eff748fe7768d
신윤섭 <zksdbstjq@gmail.com>
2026-03-17 18:17:32 +0900

    ♻️ refactor: SessionAuthFilter에 @Component 추가
    - Spring의 컴포넌트 스캔을 위해 @Component 어노테이션 추가
    - 필터가 스프링 컨텍스트에 등록되도록 변경

--- backend/src/main/java/com/zud/backend/common/filter/SessionAuthFilter.java
+++ backend/src/main/java/com/zud/backend/common/filter/SessionAuthFilter.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.zud.backend.common.util.CookieUtils;
@@ -19,6 +20,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
+@Component
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class SessionAuthFilter extends OncePerRequestFilter {
 
