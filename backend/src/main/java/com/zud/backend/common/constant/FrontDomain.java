80bc94e8a1d5702455f821be7dd74a07989edf6f
신윤섭 <zksdbstjq@gmail.com>
2026-03-18 13:42:11 +0900

    🔧 config: CORS 설정에 에이전트 도메인 추가
    Ref: #S14P21A406-162

--- backend/src/main/java/com/zud/backend/common/constant/FrontDomain.java
+++ backend/src/main/java/com/zud/backend/common/constant/FrontDomain.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public enum FrontDomain {
 
-	LOCAL("http://localhost:3000", "프론트 로컬 도메인");
+	LOCAL("http://localhost:3000", "프론트 로컬 도메인"),
+	AGENT("http://localhost:4000", "에이전트 도메인");
 
 	private final String url;
 	private final String description;
