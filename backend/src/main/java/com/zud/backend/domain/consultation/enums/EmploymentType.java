f98167d4294ea969b0a0cce5a755b60b688f6c73
신윤섭 <zksdbstjq@gmail.com>
2026-03-19 17:54:39 +0900

    ✨ feat: 고용 유형에 필수 문서 태그 추가
    - EMPLOYEE와 SELF_EMPLOYED에 필수 문서 태그 추가
    
    Ref: #S14P21A406-57

--- backend/src/main/java/com/zud/backend/domain/consultation/enums/EmploymentType.java
+++ backend/src/main/java/com/zud/backend/domain/consultation/enums/EmploymentType.java
@@ -1,5 +1,9 @@
 package com.zud.backend.domain.consultation.enums;
 
+import java.util.List;
+
+import com.zud.backend.domain.document.enums.DocumentTag;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -7,8 +11,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum EmploymentType {
-	EMPLOYEE("근로자"),
-	SELF_EMPLOYED("개인 사업자");
+	EMPLOYEE("근로자", List.of(DocumentTag.FILE_006_WITHHOLDING_TAX_CERTIFICATE)),
+	SELF_EMPLOYED("개인 사업자", List.of(DocumentTag.FILE_008_INCOME_AMOUNT_CERTIFICATE));
 
 	private final String description;
+	private final List<DocumentTag> requiredDocumentTags;
 }
