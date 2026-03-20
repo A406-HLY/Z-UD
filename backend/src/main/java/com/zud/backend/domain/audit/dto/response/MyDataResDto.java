311c02db1fae723860b3469bff90b7b8d89676c3
양예현 <yhyh1216@gmail.com>
2026-03-20 09:56:37 +0900

    ♻️ refactor: audit에 대한 리네이밍
    - Ref: #S14P21A406-123

--- backend/src/main/java/com/zud/backend/domain/audit/dto/response/MyDataResDto.java
+++ backend/src/main/java/com/zud/backend/domain/audit/dto/response/MyDataResDto.java
@@ -1,13 +1,15 @@
 package com.zud.backend.domain.audit.dto.response;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record MyDataResDto(
 	String userId,
 	String ratingName,
 
-	@JsonProperty("loanProduct")
-	LoanProductResDto loanProduct
+	@JsonProperty("loanProducts")
+	List<LoanProductResDto> loanProducts
 ) {
 	public record LoanProductResDto(
 		String accountNo,
