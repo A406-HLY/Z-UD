2b261d0b9a9ed7197990d2e8e17533c5ee6fc27b
신윤섭 <zksdbstjq@gmail.com>
2026-03-24 14:51:33 +0900

    🎉 init: SSO 인증 서버 초기 설정 및 기본 파일 추가
    Ref: #S14P21A406-183, #S14P21A406-184

---
+++ auth/src/main/java/com/bank/auth/ZudAuthApplication.java
@@ -0,0 +1,13 @@
+package com.bank.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZudAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZudAuthApplication.class, args);
+	}
+
+}
