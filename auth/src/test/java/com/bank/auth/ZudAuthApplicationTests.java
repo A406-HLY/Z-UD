1991a7840334e421ad87ad123a748670038980fb
신윤섭 <zksdbstjq@gmail.com>
2026-03-24 20:44:42 +0900

    ♻️ refactor: 테스트 클래스에서 SpringBootTest 제거
    Ref: #S14P21A406-183

--- auth/src/test/java/com/bank/auth/ZudAuthApplicationTests.java
+++ auth/src/test/java/com/bank/auth/ZudAuthApplicationTests.java
@@ -1,11 +1,8 @@
 package com.bank.auth;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ZudAuthApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
