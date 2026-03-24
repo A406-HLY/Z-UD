b6f4ec602d7903a8d66161b63427e4826c4c1545
신윤섭 <zksdbstjq@gmail.com>
2026-03-24 19:47:39 +0900

    ✨ feat: 사용자 엔티티 및 예외 처리 추가
    Ref: #S14P21A406-183, #S14P21A406-185

---
+++ auth/src/main/java/com/bank/user/entity/User.java
@@ -0,0 +1,38 @@
+package com.bank.user.entity;
+
+import com.bank.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "employee_number", unique = true, length = 100, nullable = false)
+	private String employeeNumber;
+
+	@Column(name = "name", length = 50, nullable = false)
+	private String name;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+}
+
