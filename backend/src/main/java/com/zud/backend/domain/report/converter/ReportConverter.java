a6dc94dd4b767db925220ecd39839b92031e82b7
양예현 <yhyh1216@gmail.com>
2026-03-24 17:32:50 +0900

    ✨ feat: converter 생성
    - Ref: #S14P21A406-127

---
+++ backend/src/main/java/com/zud/backend/domain/report/converter/ReportConverter.java
@@ -0,0 +1,40 @@
+package com.zud.backend.domain.report.converter;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Component;
+
+import com.zud.backend.domain.report.dto.message.LoanReportReqMessage;
+import com.zud.backend.domain.report.dto.request.LoanReportReqDto;
+import com.zud.backend.domain.report.dto.response.LoanReportGenerateRes;
+import com.zud.backend.domain.report.dto.response.LoanReportResultResDto;
+import com.zud.backend.domain.report.redis.LoanReportResultCache;
+
+@Component
+public class ReportConverter {
+
+	private static final String REPORT_REQUEST_ACCEPTED_MESSAGE = "리포트 생성 요청이 접수되었습니다.";
+
+	public LoanReportReqMessage toMessage(LoanReportReqDto request) {
+		return new LoanReportReqMessage(
+			LocalDateTime.now(),
+			request
+		);
+	}
+
+	public LoanReportGenerateRes toGenerateResponse(String uuid) {
+		return new LoanReportGenerateRes(
+			uuid,
+			REPORT_REQUEST_ACCEPTED_MESSAGE
+		);
+	}
+
+	public LoanReportResultResDto toResultResponse(LoanReportResultCache cache) {
+		return new LoanReportResultResDto(
+			cache.uuid(),
+			cache.status().name(),
+			cache.payload()
+		);
+	}
+
+}
