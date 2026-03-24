b0909d64873be63ef38a6f49cb66dab8364cb7f9
양예현 <yhyh1216@gmail.com>
2026-03-24 20:01:08 +0900

    ✨ feat: kafka 레포트 consumer 정의
    - Ref: #S14P21A406-127

--- backend/src/main/java/com/zud/backend/domain/report/service/kafka/ReportKafkaConsumer.java
+++ backend/src/main/java/com/zud/backend/domain/report/service/kafka/ReportKafkaConsumer.java
@@ -1,11 +1,26 @@
 package com.zud.backend.domain.report.service.kafka;
 
+import static com.zud.backend.common.kafka.KafkaTopics.*;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import com.zud.backend.domain.report.service.ReportResultSaveService;
 
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class ReportKafkaConsumer {
+
+	private final ReportResultSaveService reportResultSaveService;
+
+	@KafkaListener(
+		topics = REPORT_RESPONSE,
+		containerFactory = "laonReportKafkaListenerContainerFactory"
+	)
+	public void consume(String messageBody) {
+		reportResultSaveService.saveReportResult(messageBody);
+	}
 
 }
